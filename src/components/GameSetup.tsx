597a8a70c4e35268ba8052295604f07810f8df3b
Ramon van Raaij <ramon.van.raaij@gmail.com>
2022-05-21 11:20:05 +0200

    now using Chakra UI component lib

--- src/components/GameSetup.tsx
+++ src/components/GameSetup.tsx
@@ -1,9 +1,21 @@
 import React, { SyntheticEvent, useContext, useRef, useState } from "react";
 
 import { GameContext } from "../store/game-context";
-import classes from "./GameSetup.module.css";
 import { PlayerEnum, Setup } from "../models/game";
 import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Radio,
+  RadioGroup,
+  VStack,
+} from "@chakra-ui/react";
+import { GrWaypoint } from "react-icons/gr";
 
 const GameSetup: React.FC = () => {
   const gameCtx = useContext(GameContext);
@@ -15,10 +27,8 @@ const GameSetup: React.FC = () => {
   const [selectedStartingPlayer, setSelectedStartingPlayer] =
     useState<PlayerEnum>(PlayerEnum.PLAYER_ONE);
 
-  const startingPlayerHandler = (event: SyntheticEvent) => {
-    setSelectedStartingPlayer(
-      () => (event.target as HTMLInputElement).value as PlayerEnum
-    );
+  const startingPlayerHandler = (value: string) => {
+    setSelectedStartingPlayer(() => value as PlayerEnum);
   };
 
   const submitHandler = (event: React.FormEvent) => {
@@ -35,40 +45,45 @@ const GameSetup: React.FC = () => {
   };
 
   return (
-    <React.Fragment>
-      <h3>Game Setup</h3>
-      <h6 className="mb-2 text-muted">Enter some info to get started!</h6>
+    <Container maxW="container.md" py={10}>
+      <Heading fontSize="5xl">Game Setup</Heading>
       <form onSubmit={submitHandler}>
-        <label htmlFor="player-one">Player One</label>
-        <input type="text" id="player-one" ref={playerOneInputRef} />
-        <label htmlFor="player-two">Player Two</label>
-        <input type="text" id="player-two" ref={playerTwoInputRef} />
-        <label htmlFor="target-score">Target Score</label>
-        <input type="number" id="target-score" ref={targetScoreInputRef} />
-        <label htmlFor="starting-player">Starting player</label>
-        <div>
-          <label>Player One</label>
-          <input
-            type="radio"
-            value={PlayerEnum.PLAYER_ONE}
-            checked={selectedStartingPlayer === PlayerEnum.PLAYER_ONE}
-            onChange={startingPlayerHandler}
-          />
-        </div>
-        <div>
-          <label>Player Two</label>
-          <input
-            type="radio"
-            value={PlayerEnum.PLAYER_TWO}
-            checked={selectedStartingPlayer === PlayerEnum.PLAYER_TWO}
-            onChange={startingPlayerHandler}
-          />
-        </div>
-        <button color="danger" className={classes.setupButton}>
-          Start Game
-        </button>
+        <VStack spacing={4} align="flex-start">
+          <FormControl>
+            <FormLabel htmlFor="player-one">Player One</FormLabel>
+            <Input type="text" id="player-one" ref={playerOneInputRef} />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="player-two">Player Two</FormLabel>
+            <Input type="text" id="player-two" ref={playerTwoInputRef} />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="target-score">Target Score</FormLabel>
+            <Input type="number" id="target-score" ref={targetScoreInputRef} />
+          </FormControl>
+          <FormControl as="fieldset">
+            <FormLabel htmlFor="starting-player">Starting player</FormLabel>
+            <RadioGroup
+              defaultValue={PlayerEnum.PLAYER_ONE}
+              onChange={startingPlayerHandler}
+            >
+              <HStack spacing="24px">
+                <Radio value={PlayerEnum.PLAYER_ONE}>Player One</Radio>
+                <Radio value={PlayerEnum.PLAYER_TWO}>Player Two</Radio>
+              </HStack>
+            </RadioGroup>
+          </FormControl>
+          <Button
+            variant="outline"
+            type="submit"
+            rounded="full"
+            rightIcon={React.createElement(GrWaypoint)}
+          >
+            Start Game
+          </Button>
+        </VStack>
       </form>
-    </React.Fragment>
+    </Container>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,25 @@ import App from "./App";
 import GameSetup from "./components/GameSetup";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Game from "./components/Game";
+import { Box, ChakraProvider, Flex, VStack } from "@chakra-ui/react";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="setup" element={<GameSetup />} />
-        <Route path="game" element={<Game />} />
-        {/*<Route path="correction" element={<Correction/>} />*/}
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <ChakraProvider theme={theme}>
+    <Flex bg="gray.100" align="center" justify="center">
+      <Box bg="white" p={6} rounded="md" w={96}>
+        <BrowserRouter>
+          <VStack as="nav">
+            <Routes>
+              <Route path="/" element={<App />}>
+                <Route path="setup" element={<GameSetup />} />
+                <Route path="game" element={<Game />} />
+                {/*<Route path="correction" element={<Correction/>} />*/}
+              </Route>
+            </Routes>
+          </VStack>
+        </BrowserRouter>
+      </Box>
+    </Flex>
+  </ChakraProvider>
 );
