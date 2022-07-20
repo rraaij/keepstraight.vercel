6666e59cc34f82983483e7f948ccd0a5ec76fc23
Ramon van Raaij <ramon.van.raaij@gmail.com>
2022-07-20 15:43:56 +0200

    vite project with tailwind (and scoretable setup)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
-
-const App: React.FC = () => {
+function App() {
   return (
-    <React.Fragment>
-      <h1>Keepstraight</h1>
-      <nav>
-        <Link to="/setup">Setup</Link> | <Link to="/game">Game</Link>
-      </nav>
-      <Outlet />
-    </React.Fragment>
+    // SCORE TABLE
+    <div className="flex flex-col m-auto p-3 h-screen">
+      <div className="basis-1/6 text-center bg-amber-100">
+        {/*HEADER: targetScore*/}
+      </div>
+      <div className="flex-grow flex flex-row py-3 bg-gray-200 border border-black">
+        <div className="flex-grow bg-blue-400">
+          {/*LEFT TABLE: player #1 name, scoretable*/}
+        </div>
+        <div className="flex-grow bg-green-400">
+          {/*RIGHT TABLE: player #2 name, scoretable*/}
+        </div>
+      </div>
+      <div className="basis-1/6 text-center bg-amber-500">
+        {/*ACTIONS: current run score, fouls, buttons <Rerack>, <Submit Score> and <Score Correction>*/}
+      </div>
+    </div>
   );
-};
+}
 
 export default App;
