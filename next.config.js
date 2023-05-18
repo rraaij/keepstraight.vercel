83b4e1e18d0d24c8f9462e5c9513d3faf89c688c
Ramon van Raaij <ramon.van.raaij@gmail.com>
2023-05-18 09:38:30 +0200

    minor refactorings

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withNextIntl = require("next-intl/plugin")("./i18n.ts");
 
 module.exports = withNextIntl({
   distDir: "build",
-  reactStrictMode: true,
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   swcMinify: true,
   experimental: {
     appDir: true,
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,8 @@
 import LanguageSelector from "@/components/language-selector";
 import { useTranslations } from "next-intl";
 
+export const revalidate = 0; // SSR, so no caching
+
 export default function Home() {
   const t = useTranslations("metadata");
   return (
